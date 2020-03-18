c6cdf6f7266763e7ac2cd1149a8ac1a1e9c0aa3f
Bob Coret <bob.coret+github@gmail.com>
2020-03-18 21:40:08 +0100

    unused code removed

--- web/public/download.php
+++ web/public/download.php
@@ -8,26 +8,20 @@ if (isset($_GET["guid"])) {
 	if(file_exists(UPLOAD_DIR.$file)) {
 		send_file(UPLOAD_DIR,$file);		
 	} else {
-		echo "$file doesn't exist";
+		echo "ERROR: $file doesn't exist";
 	}
 } else {
-	echo "invalid guid";
+	echo "ERROR: invalid guid";
 }
 
 function send_file($dir,$filename) {
-
 	if($fn=fopen($dir.$filename,'r')) {
-		if ($bgzip>0) {
-			header('Content-Encoding: gzip');
-		}
 		header("Content-Length: ".filesize($dir.$filename));
 		header("Content-Type: text/turtle");
 		header('Content-Disposition: attachment; filename="'.$filename.'"');
 		header('Pragma: no-cache');
 		header('Connection: close');
-		#sleep(1);
 		fpassthru($fn);
 		exit;
 	}
 }
-
