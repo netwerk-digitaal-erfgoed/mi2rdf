f9baf8e354a9543e46dca6c326da5c2852e5ac66
Bob Coret <bob.coret+github@gmail.com>
2020-03-18 23:14:31 +0100

    adding header (nocache/json)

--- web/public/datasets.php
+++ web/public/datasets.php
@@ -3,5 +3,10 @@
 include('includes/config.php');
 include('includes/database.php');
 
-$datasets=arrGetDatasets(10);
+$datasets=arrGetDatasets(20);
+
+header('Content-Type: application/json');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 echo json_encode($datasets);
