c8d58599dd9492907abe07f157747005d8adf5a5
Bob Coret <bob.coret+github@gmail.com>
2020-03-23 23:57:47 +0100

    responsive design

--- web/public/index.php
+++ web/public/index.php
@@ -27,221 +27,210 @@
     </nav>
     <div class="container">
         <div class="row">
-            <div class="col" style="text-align:center;">
+            <div class="col headcol">
                 <h1>MI2RDF</h1>
                 <p>LODwrapper om MDWS Internet bestanden te converteren naar triples.</p>
-                <p><br><br></p>
             </div>
         </div>
 
         <div class="row" id="drop-area1">
             <div class="col sink storyBanner">
 
-                <table width="100%" border=0>
-                    <tr>
-                        <td id="drop-area2" rowspan="3" width="25%">
-                            <p>Sleep hier &eacute;&eacute;n of meerdere MDWS Internet bestanden heen of klik op onderstaande knop om bestanden te selecteren.</p>
-                            <form class="my-form">
-                                <input type="file" multiple id="fileElem" accept=".txt,.zip" onchange="handleFiles(this.files)">
-                                <label title="Maximale grootte per bestand is 100MB. De .txt bestanden kunnen ook gecomprimeerd en/of gebundeld worden in een .zip bestand." class="btn btn-block btn-label" for="fileElem">Selecteer bestand(en)</label>
-                            </form>
-							<br>Het vertrippelen start direct.
-                        </td>
-                        <td width="30%"><br></td>
-                        <td id="datasetlist" width="45%" rowspan="3"><br></td>
-                    </tr>
-                    <tr>
-                        <td id="midelbar"><br></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <br>
-                        </td>
-                    </tr>
-                </table>
+				<div id="drop-area2">
+					<p>Sleep hier &eacute;&eacute;n of meerdere MDWS Internet bestanden heen of klik op onderstaande knop om bestanden te selecteren.</p>
+					<form class="my-form">
+						<input type="file" multiple id="fileElem" accept=".txt,.zip" onchange="handleFiles(this.files)">
+						<label title="Maximale grootte per bestand is 100MB. De .txt bestanden kunnen ook gecomprimeerd en/of gebundeld worden in een .zip bestand." class="btn btn-block btn-label" for="fileElem">Selecteer bestand(en)</label>
+					</form>
+					<br>Het vertrippelen start direct.
+				</div>
+				<div id="midelbar">
+					<div id="koppelstuk"></div>
+	                <div class="baton-container" style="position:absolute;left:295px;top:0px;">
+						<div class="baton-0">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-1">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-2">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-3">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-4">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-5">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-6">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-7">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-8">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-9">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-10">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-11">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-12">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-13">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-14">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-15">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-16">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-17">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-18">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-19">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-20">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-21">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-22">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-23">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-24">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-25">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-26">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-27">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-28">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-29">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-30">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-31">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-32">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-33">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-34">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+						<div class="baton-35">
+							<div class="metronome">
+								<div class="baton"></div>
+							</div>
+						</div>
+					</div>
 
-                <div class="baton-container" style="position:absolute;left:27%;top:0px;">
-                    <div class="baton-0">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-1">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-2">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-3">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-4">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-5">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-6">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-7">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-8">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-9">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-10">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-11">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-12">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-13">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-14">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-15">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-16">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-17">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-18">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-19">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-20">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-21">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-22">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-23">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-24">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-25">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-26">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-27">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-28">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-29">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-30">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-31">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-32">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-33">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-34">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                    <div class="baton-35">
-                        <div class="metronome">
-                            <div class="baton"></div>
-                        </div>
-                    </div>
-                </div>
+				</div>
+				<div id="datasetlist"></div>
             </div>
         </div>
     </div>
